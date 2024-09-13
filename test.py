4b4a63f637e4c34d2fde006ebc03541746e27bdc
sebstianzamora <69611081+sebstianzamora@users.noreply.github.com>
2024-09-13 11:56:00 -0500

    solution

--- test.py
+++ test.py
@@ -6,26 +6,26 @@ import pandas as pd
 
 if not os.path.exists("train_dataset.csv"):
     raise FileNotFoundError("File 'train_dataset.csv' not found")
+def test_lab5():
+    train_dataset = pd.read_csv("train_dataset.csv")
 
-train_dataset = pd.read_csv("train_dataset.csv")
+    assert train_dataset.columns[0] == "phrase"
+    assert train_dataset.columns[1] == "sentiment"
 
-assert train_dataset.columns[0] == "phrase"
-assert train_dataset.columns[1] == "sentiment"
+    counts = train_dataset["sentiment"].value_counts()
 
-counts = train_dataset["sentiment"].value_counts()
-
-assert counts["neutral"] == 1117
-assert counts["positive"] == 458
-assert counts["negative"] == 236
+    assert counts["neutral"] == 1117
+    assert counts["positive"] == 458
+    assert counts["negative"] == 236
 
 
-if not os.path.exists("test_dataset.csv"):
-    raise FileNotFoundError("File 'test_dataset.csv' not found")
+    if not os.path.exists("test_dataset.csv"):
+        raise FileNotFoundError("File 'test_dataset.csv' not found")
 
-test_dataset = pd.read_csv("test_dataset.csv")
+    test_dataset = pd.read_csv("test_dataset.csv")
 
-counts = test_dataset["sentiment"].value_counts()
+    counts = test_dataset["sentiment"].value_counts()
 
-assert counts["neutral"] == 274
-assert counts["positive"] == 112
-assert counts["negative"] == 67
+    assert counts["neutral"] == 274
+    assert counts["positive"] == 112
+    assert counts["negative"] == 67
